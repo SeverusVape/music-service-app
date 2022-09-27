c19d8744eb4e57c23682e2138ec7d48d07eaa8af
Veter v Golove <koshelevworkmail@gmail.com>
2022-09-27 17:45:52 -0400

    Loader and Error components

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Error = () => (
-  <div>Error</div>
+    <div className="w-full flex justify-center items-center">
+        <h1 className="text-gray-300 font-bold text-2xl mt-2">
+            Something went wrong. Try again later...
+        </h1>
+    </div>
 );
 
 export default Error;
