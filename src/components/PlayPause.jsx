14fab9a3e97349ae31ff9e7086d456a14d792dfa
Veter v Golove <koshelevworkmail@gmail.com>
2022-09-27 19:20:33 -0400

    Song card components

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,10 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+    isPlaying && activeSong?.title === song.title ? (
+        <FaPauseCircle className="text-green" size={40} onClick={handlePause} />
+    ) : (
+        <FaPlayCircle className="text-green" size={40} onClick={handlePlay} />
+    );
 
 export default PlayPause;
