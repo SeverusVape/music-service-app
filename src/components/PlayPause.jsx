b0810444932a90d6a5ddcc18bb6203d7bd1f3e10
Veter v Golove <koshelevworkmail@gmail.com>
2022-09-29 17:07:52 -0400

    Top artists functionality

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -2,9 +2,9 @@ import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
 
 const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
     isPlaying && activeSong?.title === song.title ? (
-        <FaPauseCircle className="text-green" size={40} onClick={handlePause} />
+        <FaPauseCircle className="text-green" size={35} onClick={handlePause} />
     ) : (
-        <FaPlayCircle className="text-green" size={40} onClick={handlePlay} />
+        <FaPlayCircle className="text-green" size={35} onClick={handlePlay} />
     );
 
 export default PlayPause;
